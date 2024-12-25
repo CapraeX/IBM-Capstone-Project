123926077e9f87cfa4305a72c4588e1da1d725c8
alejandro99apple <alejandro99apple@gmail.com>
2024-12-25 17:59:56 -0600

    third module commit

---
+++ server/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const mongoURI =  "mongodb+srv://alejandro99apple:NpMvqWFDl6oitE3Q@cluster0.it4xx.mongodb.net";
+
+const connectToMongo = async (retryCount) => {
+    const MAX_RETRIES = 3;
+    const count = retryCount ?? 0;
+    try {
+        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
+        console.info('Connected to Mongo Successfully')
+
+        return;
+    } catch (error) {
+        console.error(error);
+
+        const nextRetryCount = count + 1;
+
+        if (nextRetryCount >= MAX_RETRIES) {
+            throw new Error('Unable to connect to Mongo!');
+        }
+
+        console.info(`Retrying, retry count: ${nextRetryCount}`)
+
+        return await connectToMongo(nextRetryCount);
+
+    }
+};
+
+module.exports = connectToMongo;
